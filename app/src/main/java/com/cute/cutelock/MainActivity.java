639f9f3db403bb77dd487a55afa9ea910d87c679
jianmngf <kenieevan@gmail.com>
2017-03-27 10:58:57 +0800

    add exit button

--- app/src/main/java/com/cute/cutelock/MainActivity.java
+++ app/src/main/java/com/cute/cutelock/MainActivity.java
@@ -2,6 +2,9 @@ package com.cute.cutelock;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button exitBn = (Button)findViewById(R.id.button);
+        exitBn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
     }
 }

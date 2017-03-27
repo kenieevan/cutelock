c05fc021b4ade4cc979872550d000d34ba9244d3
jianmngf <kenieevan@gmail.com>
2017-03-27 18:28:48 +0800

    home screen can trigger home chooser now. so you can exit the screen

--- app/src/main/java/com/cute/cutelock/MainActivity.java
+++ app/src/main/java/com/cute/cutelock/MainActivity.java
@@ -8,17 +8,22 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    protected void resetLauncher() {
+        this.getPackageManager().clearPackagePreferredActivities(this.getPackageName());
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button exitBn = (Button)findViewById(R.id.button);
+
         exitBn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+                resetLauncher();
                 finish();
-                System.exit(0);
+                //System.exit(0);
             }
         });
     }

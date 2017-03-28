eb923c9ac62f067234ce014d4df1a67e299c918f
jianmngf <kenieevan@gmail.com>
2017-03-29 06:26:10 +0800

    lock the status bar now

--- app/src/main/java/com/cute/cutelock/MainActivity.java
+++ app/src/main/java/com/cute/cutelock/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        startLockTask();
         startBlockApps();
 
 
